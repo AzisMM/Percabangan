44c414c926aefdc4bc3f3543a4ad9939acf38355
AzisMM <mabdulaziz124@gmail.com>
2021-03-09 14:21:39 +0700

    Initial commit

---
+++ src/com/azis/javafundamental/IfThen.java
@@ -0,0 +1,13 @@
+package com.azis.javafundamental;
+
+public class IfThen {
+
+    public static void main(String[] args) {
+
+        boolean isOn = true;
+
+        if (isOn) {
+            System.out.println("Menyalakan Lampu");
+        }
+    }
+}
---
+++ src/com/azis/javafundamental/IfThenElse.java
@@ -0,0 +1,14 @@
+package com.azis.javafundamental;
+
+public class IfThenElse {
+
+    public static void main(String[] args) {
+        boolean isOn = false;
+
+        if (isOn) {
+            System.out.println("Menyalakan Lampu");
+        } else {
+            System.out.println("Kondisi tidak terpenuhi");
+        }
+    }
+}
